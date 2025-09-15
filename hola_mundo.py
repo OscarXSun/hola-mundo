d8647c69a1fd5cd26e5a66886bb329d89bf9c8bf
Oscar Sun <oscar258254@gmail.com>
2025-09-15 12:32:42 -0600

    1

--- hola_mundo.py
+++ hola_mundo.py
@@ -1,8 +1,1 @@
 print("Hola Mundo en Python")
-print("Hola Mundo desde Python!")
-git init
-git add hola_mundo.py
-git commit -m "Primer commit: Hola mundo en Python"
-git branch -M main
-git remote add origin https://github.com/OscarXSun/hola-mundo.git
-git push -u origin main
